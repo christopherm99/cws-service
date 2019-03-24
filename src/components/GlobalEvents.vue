52b045f0e5a76b9d827396ee99463d452972ffcf
Christopher Milan <chris.m.milan@gmail.com>
2019-03-23 20:24:49 -0400

    feat(events): Add un-RSVP option to GlobalEvents
    Add switch between RSVP and un-RSVP for GlobalEvents dialog.

--- src/components/GlobalEvents.vue
+++ src/components/GlobalEvents.vue
@@ -1,26 +1,31 @@
 <template>
   <v-container>
-    <v-dialog v-model="dialog" max-width="500" persistent>
+    <v-dialog v-model="dialog.enabled" max-width="500" persistent>
       <v-card>
         <v-card-title primary-title class="headline">
-          Confirm Attendance
+          {{ dialog.title }}
         </v-card-title>
         <v-card-text>
-          Would you like to confirm your attendance to the following event?
+          {{ dialog.prompt }}
         </v-card-text>
         <v-divider />
         <v-card-actions>
           <v-spacer />
-          <v-btn color="accent" flat @click="dialog = false">
-            Cancel
+          <v-btn
+            color="accent"
+            flat
+            :disabled="dialog.loading"
+            @click="cleanDialog"
+          >
+            Go Back
           </v-btn>
           <v-btn
             color="accent"
             outline
-            :loading="dialogLoading"
-            @click="confirm"
+            :loading="dialog.loading"
+            @click="dialog.fun"
           >
-            Confirm
+            {{ dialog.btn }}
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -41,13 +46,10 @@
           <td>{{ props.item.hours }}</td>
           <td>{{ props.item.when }}</td>
           <td class="justify-center layout px-0">
-            <v-icon
-              flat
-              @click="
-                RSVP = props.item.id;
-                dialog = true;
-              "
-            >
+            <v-icon v-if="props.item.rsvp" flat @click="respond(props.item)">
+              cancel
+            </v-icon>
+            <v-icon v-else flat @click="respond(props.item)">
               reply
             </v-icon>
           </td>
@@ -65,6 +67,7 @@ let eventCol = db.collection("/events");
 export default {
   data() {
     return {
+      username: "",
       headers: [
         {
           text: "Organization",
@@ -90,32 +93,52 @@ export default {
       events: [],
       error: "",
       loading: true,
-      dialog: false,
-      dialogLoading: false,
-      RSVP: undefined
+      dialog: {
+        enabled: false,
+        loading: false,
+        title: "",
+        prompt: "",
+        btn: "",
+        fun: undefined,
+        meta: undefined
+      }
     };
   },
   mounted() {
+    this.username = auth.currentUser.email
+      .substring(0, auth.currentUser.email.lastIndexOf("@"))
+      .toLowerCase();
     eventCol.where("when", ">", new Date()).onSnapshot(
-      col => {
-        this.loading = false;
+      colSnap => {
         this.events = [];
-        col.forEach(doc => {
-          let data = doc.data();
-          let when = new Date(data.when.seconds * 1000);
-          this.events.push({
-            provider: data.provider,
-            hours: `${data.hours} hours`,
-            author: data.author,
-            when: `${new Intl.DateTimeFormat("en-US", {
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-              hour: "numeric",
-              minute: "numeric"
-            }).format(when)}`,
-            id: doc.id
-          });
+        colSnap.docs.forEach(doc => {
+          doc.ref
+            .collection("who")
+            .doc(this.username)
+            .get()
+            .then(rsvp => {
+              this.loading = false;
+              let data = doc.data();
+              let when = new Date(data.when.seconds * 1000);
+              this.events.push({
+                provider: data.provider,
+                hours: `${data.hours} hours`,
+                author: data.author,
+                when: `${new Intl.DateTimeFormat("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                  hour: "numeric",
+                  minute: "numeric"
+                }).format(when)}`,
+                id: doc.id,
+                rsvp: rsvp.exists
+              });
+              return;
+            })
+            .catch(error => {
+              this.error = error;
+            });
         });
       },
       error => {
@@ -124,27 +147,65 @@ export default {
     );
   },
   methods: {
-    confirm() {
-      this.dialogLoading = true;
+    respond(event) {
+      event.rsvp
+        ? (this.dialog = {
+            enabled: true,
+            loading: false,
+            title: "Cancel Attendance",
+            prompt:
+              "Would you like to cancel your attendance to the following event?",
+            btn: "Cancel",
+            fun: this.cancel,
+            meta: event.id
+          })
+        : (this.dialog = {
+            enabled: true,
+            loading: false,
+            title: "Confirm Attendance",
+            prompt:
+              "Would you like to confirm your attendance to the following event?",
+            btn: "Confirm",
+            fun: this.confirm,
+            meta: event.id
+          });
+    },
+    cleanDialog() {
+      this.dialog = {
+        enabled: false,
+        loading: false,
+        title: "",
+        prompt: "",
+        btn: "",
+        fun: () => {},
+        meta: undefined
+      };
+    },
+    cancel() {
+      this.dialog.loading = true;
       eventCol
-        .doc(this.RSVP)
+        .doc(this.dialog.meta)
         .collection("who")
-        .doc(
-          auth.currentUser.email
-            .substring(0, auth.currentUser.email.lastIndexOf("@"))
-            .toLowerCase()
-        )
+        .doc(this.username)
+        .delete()
+        .then(this.cleanDialog)
+        .catch(error => {
+          this.cleanDialog();
+          this.error = error.message;
+        });
+    },
+    confirm() {
+      this.dialog.loading = true;
+      eventCol
+        .doc(this.dialog.meta)
+        .collection("who")
+        .doc(this.username)
         .set({
           when: new Date()
         })
-        .then(() => {
-          this.dialog = false;
-          this.dialogLoading = false;
-          return;
-        })
+        .then(this.cleanDialog)
         .catch(error => {
-          this.dialog = false;
-          this.dialogLoading = false;
+          this.cleanDialog();
           this.error = error.message;
         });
     }
