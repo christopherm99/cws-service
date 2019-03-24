a148b7e9c192ec934ca670c3a8666bc31415452f
Christopher Milan <chris.m.milan@gmail.com>
2019-03-24 15:58:42 -0400

    feat(cloud-functions): Add email notifications
    Add email notifications for event authors in cloud functions with
    notifyAuthorCancellation() and notifyAuthorConfirmation()

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,19 @@
+"use strict";
+
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+
+const nodemailer = require("nodemailer");
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const mailTransport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword
+  }
+});
+const APP_NAME = "Commonwealth School Community Service";
 
 admin.initializeApp();
 
@@ -29,4 +43,78 @@ exports.incrementHours = functions.firestore
         console.error(`Cannot retrieve user's current hours: ${error}`);
         throw error;
       });
+  });
+
+function sendEmail(email, subject, text) {
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@firebase.com>`,
+    to: email
+  };
+  mailOptions.subject = subject;
+  mailOptions.text = text;
+  return mailTransport.sendMail(mailOptions);
+}
+
+exports.notifyAuthorConfirmation = functions.firestore
+  .document("events/{eventID}/who/{username}")
+  .onCreate((snap, ctx) => {
+    console.log(
+      `User ${ctx.params.username} has confirmed their attendance at event ${
+        ctx.params.eventID
+      }.`
+    );
+    let eventdoc = snap.ref.parent.parent;
+    return eventdoc.get().then(doc => {
+      let eventdata = doc.data();
+      let authormail = eventdata.author + "@commschool.org";
+      console.log(`Notifying ${authormail} of confirmation.`);
+      return sendEmail(
+        authormail,
+        `${ctx.params.username} has joined your event`,
+        `Dear ${eventdata.author},
+User ${ctx.params.username} has joined your event at ${
+          eventdata.provider
+        } on ${new Intl.DateTimeFormat("en-US", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "numeric",
+          minute: "numeric"
+        }).format(new Date(eventdata.when.seconds * 1000))}.
+From,
+  The Commonwealth Community Service Team`
+      );
+    });
+  });
+
+exports.notifyAuthorCancellation = functions.firestore
+  .document("events/{eventID}/who/{username}")
+  .onDelete((snap, ctx) => {
+    console.log(
+      `User ${ctx.params.username} has cancelled their attendance at event ${
+        ctx.params.eventID
+      }.`
+    );
+    let eventdoc = snap.ref.parent.parent;
+    return eventdoc.get().then(doc => {
+      let eventdata = doc.data();
+      let authormail = eventdata.author + "@commschool.org";
+      console.log(`Notifying ${authormail} of cancellation.`);
+      return sendEmail(
+        authormail,
+        `${ctx.params.username} has left your event`,
+        `Dear ${eventdata.author},
+User ${ctx.params.username} has left your event at ${
+          eventdata.provider
+        } on ${new Intl.DateTimeFormat("en-US", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "numeric",
+          minute: "numeric"
+        }).format(new Date(eventdata.when.seconds * 1000))}.
+From,
+  The Commonwealth Community Service Team`
+      );
+    });
   });
