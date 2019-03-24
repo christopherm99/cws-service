c8408249db3b906086d6d51c172896f4bfddfbc1
Christopher Milan <chris.m.milan@gmail.com>
2019-03-23 22:22:08 -0400

    feat(cloud-functions): Add incrementHours()
    Add incrementHours() function, reacting to changes to /users/_/events/_.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,32 @@
-// eslint-disable-next-line no-unused-vars
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.incrementHours = functions.firestore
+  .document("users/{username}/events/{eventID}")
+  .onCreate((snap, ctx) => {
+    console.log(
+      `User ${ctx.params.username} created an event: ${ctx.params.eventID}`
+    );
+    let userdoc = snap.ref.parent.parent;
+    return userdoc
+      .get()
+      .then(doc => {
+        let hours = doc.data().hours;
+        if (!hours) {
+          hours = 0;
+        }
+        console.log(
+          `User ${ctx.params.username} currently has ${hours} hours logged.`
+        );
+        console.log(`This event will add ${snap.data.hours} more hours`);
+        return userdoc.update({
+          hours: hours + snap.data.hours
+        });
+      })
+      .catch(error => {
+        console.error(`Cannot retrieve user's current hours: ${error}`);
+        throw error;
+      });
+  });
